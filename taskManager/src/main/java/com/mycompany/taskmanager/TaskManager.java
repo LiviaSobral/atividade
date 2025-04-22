38ac3eb73610671d9373aa4bd1c18d3fc731ab74
LiviaSobral <134714074+LiviaSobral@users.noreply.github.com>
2025-04-22 16:50:32 -0300

    Add files via upload

---
+++ taskManager/src/main/java/com/mycompany/taskmanager/TaskManager.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taskmanager;
+
+import com.mycompany.taskmanager.dao.TaskDAO;
+import com.mycompany.taskmanager.dao.UserDAO;
+import com.mycompany.taskmanager.database.ConnectionSQL;
+import com.mycompany.taskmanager.model.*;
+
+/**
+ *
+ * @author LIVIAHELOISAALVESSOB
+ */
+public class TaskManager {
+
+    public static void main(String[] args) {
+        ConnectionSQL.connect();       
+        User user = new User("Roberto Carlos","robertoCarlos@mail","1234");
+//        UserDAO.registerUser(user);
+        Task task = new Task("Comprar pão","Ir na padaria compra pão","22/04/2025");
+        TaskDAO.registerNewTask(task, 1);
+//        TaskDAO.updateStatus(1, true);
+//          TaskDAO.deleteTask(1);
+        
+        
+    }
+}
