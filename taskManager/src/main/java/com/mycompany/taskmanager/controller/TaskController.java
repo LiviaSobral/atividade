b7f67010ab61306d001fddf304bb3971fae73b66
LiviaSobral <134714074+LiviaSobral@users.noreply.github.com>
2025-04-23 16:27:46 -0300

    Add files via upload

---
+++ taskManager/src/main/java/com/mycompany/taskmanager/controller/TaskController.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taskmanager.controller;
+
+import com.mycompany.taskmanager.dao.TaskDAO;
+import com.mycompany.taskmanager.model.Task;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LIVIAHELOISAALVESSOB
+ */
+public class TaskController {
+    private static ArrayList<Task> userTasks = new ArrayList<>();
+    
+    public static boolean registerTask(String title,String description,String strDate, int userID){
+        java.util.Date date1;
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            try{                
+            date1 = sdf.parse(strDate);
+            }catch(ParseException e){
+                System.out.println("Error: " + e.getMessage());
+                return false;
+            }
+            java.sql.Date date = new java.sql.Date(date1.getTime());
+            return TaskDAO.registerNewTask(title, description, date, userID);
+    }
+    
+    public static boolean updateStatus(int index, boolean Status){
+        return TaskDAO.updateStatus(getTaskByIndex(index).getId(), Status);
+    }
+    
+    public static boolean deleteTask(int index){
+        return TaskDAO.deleteTask(getTaskByIndex(index).getId());
+    }
+    
+    public static ArrayList<String> findTasks(int userID){
+        ArrayList<String> tasksStr = new ArrayList<>();
+        
+        userTasks = TaskDAO.listTasksOfUser(userID);
+        for(Task task : userTasks){
+            String status;
+            if(task.getStatus()){
+                status = "Finished";
+            }else{
+                status = "Unfinished";
+            }
+        String info = "ID: " + task.getId()+
+                " | Title: "+ task.getTitle()+
+                " | Date: "+ task.getExpirationDate()+
+                " | Status: "+status;
+        tasksStr.add(info);
+    }
+        return tasksStr;        
+    }
+    public static ArrayList<String> findTasks(int userID,boolean boolStatus){
+        ArrayList<String> tasksStr = new ArrayList<>();
+        String strStatus = "";
+        if(boolStatus){
+            strStatus = "finished";
+        }else{
+            strStatus = "unfinished";
+        }
+        userTasks = TaskDAO.listTasksOfUser(userID,strStatus);
+        for(Task task : userTasks){
+            String status;
+            if(task.getStatus()){
+                status = "finished";
+            }else{
+                status = "unfinished";
+            }
+        String info = "ID: " + task.getId()+
+                " | Title: "+ task.getTitle()+
+                " | Date: "+ task.getExpirationDate()+
+                " | Status: "+status;
+        tasksStr.add(info);
+    }
+        return tasksStr;        
+    }
+    
+    public static String getTaskDesc(int index){
+        return TaskDAO.returnTask(getTaskByIndex(index).getId());
+    }
+    
+    private static Task getTaskByIndex(int index){
+        if(index >= 0 && index < userTasks.size()){
+            return userTasks.get(index);
+        }
+        return null;
+    }
+    
+}
---
+++ taskManager/src/main/java/com/mycompany/taskmanager/model/Task.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taskmanager.model;
+
+/**
+ *
+ * @author LIVIAHELOISAALVESSOB
+ */
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private String expirationDate;
+    private boolean status;
+
+    public Task(String title,String description, String expirationDate) {
+        this.title = title;
+        this.description = description;
+        this.expirationDate = expirationDate;
+        this.status = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }    
+    
+}
